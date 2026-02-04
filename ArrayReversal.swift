c2231cc536eb91b5513d4f848b5ece538affd62b
aswinrathees <aswinrathees.tech@gmail.com>
2026-02-04 13:29:59 +0530

    ArrayReversal-1DArray

---
+++ ArrayReversal.swift
@@ -0,0 +1,19 @@
+class ArrayReversal {
+
+	func reverse(_ s: inout [Character]) {
+		var left = 0
+		var right = s.count - 1
+		
+		while left < right {
+			s.swapAt(left, right)
+			left += 1
+			right -= 1
+		}
+
+    print("Reverse: \(s)")
+	}
+}
+
+let arrayReversal = ArrayReversal()
+var inputString: [Character] = ["h", "e", "l", "l", "o"]
+arrayReversal.reverse(&inputString)
