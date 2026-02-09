3b489441a8b760aaa8da887d3d859969a546bb9f
Aswin Rathees <33830946+aswinrathees@users.noreply.github.com>
2026-02-09 23:32:46 +0530

    StringDifference2

---
+++ StringDifference2.swift
@@ -0,0 +1,24 @@
+class StringDifference2 {
+
+    func findTheDifference(_ s: String, _ t: String) -> Character {
+        var count = [Character: Int]()
+    
+        // Count characters in both strings
+        for char in s {
+            count[char, default: 0] += 1
+        }
+        for char in t {
+            count[char, default: 0] -= 1
+            if count[char]! == -1 {
+                return char  // Found the extra character
+            }
+        }
+        
+        return " "
+    }
+
+}
+
+let stringDifference2 = StringDifference2()
+let result = stringDifference2.findTheDifference("abcd", "abcde")
+print(result)
