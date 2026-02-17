e7f7611d70fcff0621f14ba3d31cc5ea15a6aeba
Aswin Rathees <33830946+aswinrathees@users.noreply.github.com>
2026-02-17 23:31:18 +0530

    Anagram-String

---
+++ Anagram.swift
@@ -0,0 +1,27 @@
+class Anagram {
+
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else {
+            return false
+        }
+
+        var charCount: [Character: Int] = [:]
+
+        for char in s {
+            charCount[char, default: 0] += 1
+        }
+
+        for char in t {
+            charCount[char, default: 0] -= 1
+            if charCount[char]! < 0 {
+                return false
+            }
+        }
+
+        return true
+    }
+}
+
+let anagram = Anagram()
+let result = anagram.isAnagram("anagram","nagaram")
+print(result)
