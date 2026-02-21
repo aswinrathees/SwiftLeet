b8eb2f22fd3b3182848d8892a6e5ec310abdccd2
Aswin Rathees <33830946+aswinrathees@users.noreply.github.com>
2026-02-21 22:24:28 +0530

    ReverseStringTwo

---
+++ ReverseStringTwo.swift
@@ -0,0 +1,23 @@
+class ReverseStringTwo {
+
+    func reverseWithKey(_ s: String, _ k: Int) {
+        var result - Array(s)
+
+        for start in stride(from: 0, to: result.count, by: 2*k) {
+            var left = start
+            var right = min(result.count - 1, start + k - 1)
+            while left < right {
+                result.swapAt(left, right)
+
+                left += 1
+                right -= 1
+            }
+        }
+
+        return String(result)
+    }
+}
+
+let reverseStringTwo = ReverseStringTwo()
+let result = reverseStringTwo.reverseWithKey("abcdefg", 2)
+print(result)
