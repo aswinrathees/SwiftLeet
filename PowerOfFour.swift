476251a8720a8ea0354a0984c81f53fdb23cd27a
Aswin Rathees <33830946+aswinrathees@users.noreply.github.com>
2026-02-26 08:27:04 +0530

    PowerOfFour

---
+++ PowerOfFour.swift
@@ -0,0 +1,19 @@
+class PowerOfFour {
+
+    func isPowerOfFour(_ n: Int) -> Bool {
+        if n <= 0 {
+            return false
+        }
+        
+        var num = n
+        while num % 4 == 0 {
+            num /= 4
+        }
+        
+        return num == 1
+    }
+}
+
+let powerOfFour = PowerOfFour()
+let result = powerOfFour.isPowerOfFour(16)
+print(result)
