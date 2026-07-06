ae11276e60179e4e1296b775a24ddf38174d2a95
Aswin Rathees <33830946+aswinrathees@users.noreply.github.com>
2026-07-06 22:28:21 +0530

    NimGameL29

--- NimGameL29.swift
+++ NimGameL29.swift
@@ -1,10 +1,10 @@
-class NimGame {
+class NimGameL29 {
 
     func canWinNim(_ n: Int) -> Bool {
         return n % 4 != 0
     }
 }
 
-let nimGame = NimGame()
+let nimGame = NimGameL29()
 let result = nimGame.canWinNim(4)
 print(result) // Output: false
